5d271d65b9b354a59101a0d46867e4cd5cce6e9d
Pungpeee <60164550+Pungpeee@users.noreply.github.com>
2023-06-01 12:52:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express();
 
 
-app.get("/health", (req, res) => {
-    res.send("Health good")
+app.get("/products", (req, res) => {
+    res.send("Milk, Ramen, Tempura, Pizza, Cake, Cookie, Orange juice, Soda")
+})
+
+app.get("/products/beverage", (req, res) => {
+    res.send("Milk, Orange juice, Soda")
+})
+
+app.get("/products/food", (req, res) => {
+    res.send("Ramen, Tempura, Pizza")
+})
+
+app.get("/products/dessert", (req, res) => {
+    res.send("Cake, Cookie")
+})
+
+app.get("/products/menu/cafe", (req, res) => {
+    res.send("Milk, Cake, Cookie")
 })
 
 app.listen(3000, () => {
